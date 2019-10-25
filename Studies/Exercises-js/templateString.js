1291ca415f82dc908bc8856003ac44b2289f68d8
Felipe D. Santos <lycan.felipe@gmail.com>
2019-10-25 11:31:31 -0200

    template string

---
+++ Studies/Exercises-js/templateString.js
@@ -0,0 +1,15 @@
+const nome = 'Rebeca'
+console.log('Olá ' + nome)
+
+const template = `
+
+Olá ${nome}
+
+`
+
+console.log(template)
+
+console.log(` 1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei....${up('cuidado')}!`)
