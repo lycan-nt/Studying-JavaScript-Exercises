ab0439b496faa9b630000ce23269ca28b150d795
Felipe D. Santos <lycan.felipe@gmail.com>
2019-10-26 11:46:25 -0200

    Objeto

---
+++ Studies/Exercises-js/fundamentos/objetos.js
@@ -0,0 +1,17 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 555.66
+prod1['Desconto Legal'] = 0.40 // Evitar Usar Espaço
+
+console.log(prod1)
+
+
+const prod2 = {
+
+nome: 'Camiseta',
+valor: 59.90,
+Desconto: 0.70
+
+}
+
+console.log(prod2)
