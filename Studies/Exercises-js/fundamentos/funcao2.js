cbe4109c56dd2ffd390160898d8340889838eb07
Felipe D. Santos <lycan.felipe@gmail.com>
2019-10-29 16:32:18 -0200

    Função 2

---
+++ Studies/Exercises-js/fundamentos/funcao2.js
@@ -0,0 +1,27 @@
+//Armazenar uma função em uma variavel
+
+const imprimirSoma = function(a,b){
+
+    console.log(a + b)
+
+}
+
+imprimirSoma(2,3)
+
+//Armazenando uma função arrow em uma variavel
+
+const soma = (a,b) => {
+
+return a + b
+
+}
+
+console.log(soma(5,5))
+
+//Retorno implicito
+
+const subtracao = (a,b) => a - b
+console.log(subtracao(5,3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!')
