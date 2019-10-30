2b298626b3f93cf02e41dfa47f743f83cfa86fb3
Felipe D. Santos <lycan.felipe@gmail.com>
2019-10-30 09:48:51 -0200

    Usando Var 1

---
+++ Studies/Exercises-js/fundamentos/usandoVar1.js
@@ -0,0 +1,26 @@
+{
+    {
+        {
+            {
+
+                var sera = 'será'
+                console.log(sera)
+
+
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+
+    var local = 'Teste'
+    console.log(local)        
+
+}
+
+teste()
+
+//console.log(local)  ----Não consegue encontrar no caso do bloco ser uma função
