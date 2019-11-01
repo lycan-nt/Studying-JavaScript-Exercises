bb493b88bc1ca0b6892364d4c50270e5f5234444
Felipe D. Santos <lycan.felipe@gmail.com>
2019-11-01 10:22:51 -0200

    Aritmeticos

---
+++ Studies/Exercises-js/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [10, 15, 16, 30]
+
+const soma = a + b + c + d
+const subtracao = c - a
+const multiplicacao =  a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
