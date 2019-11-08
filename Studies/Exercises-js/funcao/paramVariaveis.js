c6973c82859c542a3dc9b81a88baa45ffebe16cb
Felipe D. Santos <lycan.felipe@gmail.com>
2019-11-08 15:58:26 -0200

    Paramentros & Variaveis

---
+++ Studies/Exercises-js/funcao/paramVariaveis.js
@@ -0,0 +1,13 @@
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+
+    return soma
+}
+
+console.log(soma(5,5,5))
+console.log(soma(1))
+console.log(soma())
+console.log(soma( 5 ,5, 'Teste'))
