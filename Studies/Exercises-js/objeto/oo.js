5b2f7fbf6cdf18ecce37ce1df369ced93c36b471
Felipe D. Santos <lycan.felipe@gmail.com>
2019-11-14 06:53:57 -0200

    Objeto

---
+++ Studies/Exercises-js/objeto/oo.js
@@ -0,0 +1,23 @@
+//Codigo não execultavel
+
+//Procedural
+processamento(valor1,valor2,valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento(){
+        //....
+    }
+}
+
+objeto.processamento()
+
+//Principios importantes
+//Abstração
+//Encapsulamento
+//Herança
+//Polimorfismo
