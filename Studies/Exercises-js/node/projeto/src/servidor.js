4149c7935a260200e2164758db195c211296591c
Felipe D. Santos <lycan.felipe@gmail.com>
2019-12-20 07:35:41 -0200

    Servidor e banco de dados

--- Studies/Exercises-js/node/projeto/src/servidor.js
+++ Studies/Exercises-js/node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados') 
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'NootBook', preco: 123.5}) //Converte para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.bady.name,
+        preco: req.bady.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
