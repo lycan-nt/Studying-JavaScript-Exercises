1d9284e47a61c1850f23636fb50e456cb1dd93be
Felipe D. Santos <lycan.felipe@gmail.com>
2019-12-22 15:03:35 -0200

    __dirname & html

--- HelloWorld/teste/app.js
+++ HelloWorld/teste/app.js
@@ -1,7 +1,7 @@
 var http = require('http')
 
 http.createServer(function(req, res){
-    res.end("Bem vindo ao meu app")
+    res.end("Bems vindo ao meu app")
 }).listen(8080) //Cria o servidor http e em seguida define a porta
 
 console.log("Servidor Online!!")
--- HelloWorld/teste/index.js
+++ HelloWorld/teste/index.js
@@ -3,11 +3,11 @@ const express = require("express")
 const app = express()
 
 app.get('/', function(req, res){
-    res.send('Hello World!!')
+    res.sendFile(__dirname + "/html/index.html")
 })
 
 app.get('/sobre', function(req, res){
-    res.send('Bem vindo ao meu app!!')
+    res.sendFile(__dirname + "/html/sobre.html")
 })
 
 app.get('/contato', function(req, res){
