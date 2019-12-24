63fecb97d980ac2a91efb0de7b8d2978e1da4781
Felipe D. Santos <lycan.felipe@gmail.com>
2019-12-24 06:40:38 -0200

    Promise

---
+++ Studies/esNext/promises.js
@@ -0,0 +1,11 @@
+function falarDepois(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepois(3, 'Que legal')
+    .then(frase => frase.concat('?!!'))
+    .then(outraFrase => console.log(outraFrase))
