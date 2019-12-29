fa9027060253c33136714ab6600ff014fc7c2aac
Felipe D. Santos <lycan.felipe@gmail.com>
2019-12-29 18:05:18 -0200

    Sequelize inserindo dados

--- HelloWorld nodeJS/teste/teste.js
+++ HelloWorld nodeJS/teste/teste.js
@@ -5,8 +5,39 @@ const sequelize = new Sequelize('test', 'root', '131216',{
 
 })
 
-sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso!")
-}).catch(function(erro){
-    console.log("Falha ao conectar " + erro)
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 })
+
+Postagem.create({
+    titulo: "Teste de titulo",
+    conteudo: "Um conteudo qualquer"
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email:{
+        type: Sequelize.STRING
+    } 
+})
+
+Usuario.create({
+    nome: "Felipe",
+    sobrenome: "D. Sanots",
+    idade: 24,
+    email: "lycan.felipe@gmail.com"
+})
+
