b01df73304a5b79ede90b7cd23e194635911f30f
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-01 18:26:48 -0200

    headlebars

--- HelloWorld nodeJS/teste/index.js
+++ HelloWorld nodeJS/teste/index.js
@@ -2,25 +2,23 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/html/index.html")
+const handlebars = require('express-handlebars')
+
+
+
+
+//config
+    //Templade Engine
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    //Conexão com o banco de
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('test', 'root', '131216',{
+    host: "localhost",
+    dialect: "mysql"
+
 })
-
-app.get('/sobre', function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html")
-})
-
-app.get('/contato', function(req, res){
-    res.send('Contatos@contatos.com')
-})
-
-
-app.get('/ola/:cargo/:nome', function(req, res) {
-    res.send("<h1>Bem vindo, " + req.params.nome + "!</h1>" + "<h2> Seu cargo e: " + req.params.cargo + ". <h2>")
-    
-})
-
-
 
 app.listen(8080, function() {
     console.log('Server online!')
