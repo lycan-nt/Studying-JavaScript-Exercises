df219629e96cca451600145c11f96b9a4559c23f
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-09 20:01:56 -0200

    Iniciando aplicaçao

--- rocketseat- CursoJS/3-App/todos.js
+++ rocketseat- CursoJS/3-App/todos.js
@@ -1,1 +1,9 @@
-alert('Hello World')
+var listElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+var todos = [
+    'Fazer Café',
+    'Estudar Javascript',
+    'Acessar'
+]
