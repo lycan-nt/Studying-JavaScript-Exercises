29ea8641a35d944195a94fb128dab89adde4215d
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-09 20:25:49 -0200

    Renderizando todos

--- rocketseat- CursoJS/3-App/todos.js
+++ rocketseat- CursoJS/3-App/todos.js
@@ -7,3 +7,15 @@ var todos = [
     'Estudar Javascript',
     'Acessar'
 ]
+
+function rederTodos(){
+    for(todo of todos){
+        var todoElement = document.createElement('li')
+        var todoText = document.createTextNode(todo)
+
+        todoElement.appendChild(todoText)
+        listElement.appendChild(todoelement)
+    }
+}
+
+rederTodos()
