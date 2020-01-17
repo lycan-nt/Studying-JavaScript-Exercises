a0666d9baee97399dda68786e4494af164aa7985
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-17 20:47:43 -0200

    Desafio 3 modulo 4 concluido

--- rocketseat- CursoJS/4-JsAssincrono/Desafios/2/main.js
+++ rocketseat- CursoJS/4-JsAssincrono/Desafios/2/main.js
@@ -3,7 +3,19 @@ let name = document.querySelector('input[name=user]')
 let elementDiv = document.querySelector('div')
 let elementUl = document.createElement('ul')
 
+
+
+
+function carregando(){
+    var carregandoElement = document.createElement('h3')
+    var textCarrega = document.createTextNode('Caregando...')
+    carregandoElement.appendChild(textCarrega)
+    elementDiv.appendChild(carregandoElement)
+}
+
+
 function Add(text){
+
     var elementLi = document.createElement('li')
     
     var elementText = document.createTextNode(text)
@@ -17,18 +29,27 @@ let btnAdd = document.querySelector('button')
 
 btnAdd.onclick = function () {
 
-        elementUl.innerHTML = ''
+    elementUl.innerHTML = ''
+    elementDiv.innerHTML = ''
+
+        carregando()        
+
+
 
         axios.get(`https://api.github.com/users/${name.value}/repos`)
             .then(function(response){
                 
                 var item = response.data
              
-                for( let i = 0; i < item.length; i++ ){
+                 setTimeout(function(){
+
+ 
+                    for( let i = 0; i < item.length; i++ ){
                   
-                   var listar = item[i].name
-                   Add(listar)
-                }
+                        var listar = item[i].name
+                        Add(listar)
+                     }
+                 }, 3000) 
                 
             })
             .catch(function(erro){
