ac003f229d70c8af870ccc1e7078fd2a51668246
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-21 15:25:49 -0300

    Const e Let

---
+++ rocketseat_cursos/2_ES6/1_Conceitos/2_ConstLet/main.js
@@ -0,0 +1,15 @@
+const usuario = { nome:'Felipe D. Santos'};
+
+usuario.nome = 'Felipe NextDev';
+
+console.log(usuario.nome);
+
+function teste(x){
+    let y =2;
+
+    if(x > 5){
+        console.log(x,y);
+    }
+}
+
+teste(10);
