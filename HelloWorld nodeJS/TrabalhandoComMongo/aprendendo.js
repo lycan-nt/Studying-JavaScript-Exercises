f931f0c43f2a72b3fcc75b67b52097cd4035d6e5
Felipe D. santos <lycan.felipe@gmail.com>
2020-01-22 20:52:14 -0300

    Conectando ao mongoose

--- HelloWorld nodeJS/TrabalhandoComMongo/aprendendo.js
+++ HelloWorld nodeJS/TrabalhandoComMongo/aprendendo.js
@@ -1,1 +1,9 @@
-const mongoose = requeri('mongoose');
+const mongoose = require('mongoose');
+
+//Configurando o mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/aprendendo", {
+    useMongoClient: true
+})
+    .then(() => console.log('Conectado com sucesso'))
+    .catch((erro) => console.log('Falaha ao conectar' + erro));
