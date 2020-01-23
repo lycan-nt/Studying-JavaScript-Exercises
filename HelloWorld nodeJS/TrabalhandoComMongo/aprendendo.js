ef9c135c2ec4c493b526c14f0b2ea9b9d5466561
Felipe D. santos <lycan.felipe@gmail.com>
2020-01-22 21:36:16 -0300

    Criando collection

--- HelloWorld nodeJS/TrabalhandoComMongo/aprendendo.js
+++ HelloWorld nodeJS/TrabalhandoComMongo/aprendendo.js
@@ -7,3 +7,47 @@ mongoose.connect("mongodb://localhost/aprendendo", {
 })
     .then(() => console.log('Conectado com sucesso'))
     .catch((erro) => console.log('Falaha ao conectar' + erro));
+
+//Model - Usúarios
+//Definindo o model
+
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+
+    sobrenome: {
+        type: String,
+        require: true
+    },
+
+    email: {
+        type: String,
+        require: true
+    },
+
+    idade: {
+        type: Number,
+        require: true
+    },
+
+    pais: {
+        type: String
+    }
+});
+
+mongoose.model('usuarios', UsuarioSchema);
+
+const user = mongoose.model('usuarios');
+
+new user({
+    nome: 'Felipe ',
+    sobrenome: 'D. santos',
+    email: 'lycan.felipe@gmail.com',
+    idade: 24,
+    pais: 'Brasil'
+
+}).save()
+        .then(() => console.log('Novo usúario cadastrado!'))
+        .catch((erro) => console.log('Falha ao cadastrar novo usúario: ' + erro));
