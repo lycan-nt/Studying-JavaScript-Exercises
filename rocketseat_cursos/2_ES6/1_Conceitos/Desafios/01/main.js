a5267030106fd5dfb7896853a7918769edae9544
Felipe D. Santos <lycan.felipe@gmail.com>
2020-01-29 15:28:55 -0300

    Desafio 1 ES6

---
+++ rocketseat_cursos/2_ES6/1_Conceitos/Desafios/01/main.js
@@ -0,0 +1,34 @@
+class Usuarios{
+    constructor(email, senha){
+        this.email = email;
+        this.senha = senha;
+        
+    }
+
+    isAdmin(){
+
+       // this.admin = '';
+        if(this.admin == true){
+            console.log('Admintrador!')
+        }else(console.log('Não é ADM'))
+       
+    }
+}
+
+class Admin extends Usuarios{
+    
+    
+    constructor(){
+        super()
+        this.admin = true;
+    }
+    
+}
+//Admin.admin = true;
+
+let user1 = new Usuarios('marcella@gmail.com', '9596');
+let user2 = new Admin('lycan.felipe@gmail.com', '131216');
+
+console.log(user1.isAdmin());
+console.log(user2.isAdmin());
+
