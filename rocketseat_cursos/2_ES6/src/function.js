768f08618ce11b5a35d6ba9c16b9093af2902e01
Felipe D. Santos <lycan.felipe@gmail.com>
2020-02-04 15:43:33 -0300

    Desafio ES6 webpack

---
+++ rocketseat_cursos/2_ES6/src/function.js
@@ -0,0 +1,7 @@
+export const idade = 23;
+
+export default class Usuario{
+    static infor() {
+        console.log('Apenas um teste');
+    }
+}
--- rocketseat_cursos/2_ES6/src/main.js
+++ rocketseat_cursos/2_ES6/src/main.js
@@ -1,1 +1,12 @@
 alert('Testando');
+
+import classUsuario from './function'
+
+import Usuario, {idade} from './function'
+
+import { Usuario, idade as usuarioIdade } from './function';
+
+console.log(idade);
+
+const teste = classUsuario.infor();
+console.log(teste);
