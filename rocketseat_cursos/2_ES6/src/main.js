ab6e2896cdb1558364b819adbfe24c11657263c5
Felipe D. Santos <lycan.felipe@gmail.com>
2020-02-13 16:35:17 -0300

    AppDevs

--- rocketseat_cursos/2_ES6/src/main.js
+++ rocketseat_cursos/2_ES6/src/main.js
@@ -1,15 +1,28 @@
-import axios from 'axios';
+class App{
+    constructor(){
+        this.repositories = [];
 
-class Api {
-    static async getUserInfo(username){
-        try{
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        console.log(response);
-        } catch(err){
-            console.log('Opá ouve um erro: ' + err)
-        }
-      
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Felipe D. Santos',
+            description: 'Tire a sua ideia do papel',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/lycan-nt',
+        });
+
+        console.log(this.repositories);
     }
 }
 
-Api.getUserInfo('lycan-nt');
+new App();
