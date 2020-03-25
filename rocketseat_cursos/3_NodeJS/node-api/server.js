db23584bd93743699da74a61bd3c370461e222a0
Felipe D. Santos <lycan.felipe@gmail.com>
2020-03-25 17:02:14 -0300

    Node API

--- rocketseat_cursos/3_NodeJS/node-api/server.js
+++ rocketseat_cursos/3_NodeJS/node-api/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o app
 const app = express();
 
+//Iniciando o DB
+mongoose.connect()
+
+//Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
