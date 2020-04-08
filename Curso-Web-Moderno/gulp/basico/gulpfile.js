8dd52a34b4f119eb121a1206ad493a79b1e2e820
Felipe D. santos <lycan.felipe@gmail.com>
2020-04-07 22:02:11 -0300

    Trabalhando com gulp 2

--- Curso-Web-Moderno/gulp/basico/gulpfile.js
+++ Curso-Web-Moderno/gulp/basico/gulpfile.js
@@ -1,9 +1,25 @@
 const gulp = require('gulp');
-const series = gulp.series;
+const {series, parallel} = require('gulp')
+
+const tarefaAntes1 = cb => {
+    console.log('Antes 1');
+    return cb();
+}
+
+const tarefaAntes2 = cb => {
+    console.log('Antes 2');
+    return cb();
+}
 
 function copiar(cb) {
-    console.log('Tarefa de copiar');
+    gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt'])
+        .pipe(gulp.dest('pastaB'))
     return cb()
 }
 
-module.exports.default = series(copiar)
+const tarefaFim = cb => {
+    console.log('Fim');
+    return cb();
+}
+
+module.exports.default = series(parallel( tarefaAntes1, tarefaAntes2 ), copiar, tarefaFim,);
