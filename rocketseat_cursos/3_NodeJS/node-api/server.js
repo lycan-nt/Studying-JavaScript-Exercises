fb8a09e034f236313a9a9ba14562b3a2dc9fb753
Felipe D. Santos <lycan.felipe@gmail.com>
2020-04-10 07:51:55 -0700

    Reestruturação de arquivos

--- rocketseat_cursos/3_NodeJS/node-api/server.js
+++ rocketseat_cursos/3_NodeJS/node-api/server.js
@@ -6,26 +6,14 @@ const requireDir = require('require-dir');
 const app = express();
 
 //Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi2', 
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
 { useNewUrlParser: true,
 useUnifiedTopology: true  }
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-
-//Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'Hello World',
-        description: 'NextDev Rumo ao proximo nivel',
-        url: 'lycan.felipe@gmail.com',
-    });
-    
-    return res.send('Hello World!');
-});
-
+//Rotas
+app.use('/api', require('./src/routes'));
 
 
 
