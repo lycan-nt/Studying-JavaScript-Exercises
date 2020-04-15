17094d683cdee4e38ed61b27e935f7990583a050
Felipe D. Santos <lycan.felipe@gmail.com>
2020-04-15 17:38:19 -0300

    Introdução

---
+++ Digital-Inovation-cursos/JavaScript/js/main.js
@@ -0,0 +1,11 @@
+var nome = "Felipe D. Santos"
+var idade = 24
+var idade2 = 10;
+var frase = "Bora dormi"
+
+//alert(`${nome} tem ${idade} anos`)
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace('dormi', 'codar!!!'));
