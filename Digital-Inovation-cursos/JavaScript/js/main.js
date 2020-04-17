f54e36d959220a712cfa50a6232227fbabdd5abd
Felipe D. Santos <lycan.felipe@gmail.com>
2020-04-17 17:25:07 -0300

    Funções e varivais global

--- Digital-Inovation-cursos/JavaScript/js/main.js
+++ Digital-Inovation-cursos/JavaScript/js/main.js
@@ -66,3 +66,32 @@
 
 // var data = new Date;
 // alert(data);
+
+
+//==========================================================================================================
+
+// function soma(n1 , n2) {
+//     return n1 + n2  
+// }
+
+// function setReplace(frase, nome, novo_nome) {
+//     return frase.replace(nome, novo_nome);
+// }
+
+// alert(setReplace("Bora dormir", "dormir", "Codar!"));
+
+// alert(soma(10,10));
+
+function validarIdade(idade) {
+    var validar;
+    if(idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validarIdade(idade));
