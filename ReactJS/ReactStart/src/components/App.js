0ca3d9bf7b4255640820516f3cb57c8b318d25af
Felipe D. Santos <lycan.felipe@gmail.com>
2020-05-13 18:02:10 -0700

    Implementing an accountant

--- ReactJS/ReactStart/src/components/App.js
+++ ReactJS/ReactStart/src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Contador from './Contador'
 
 export default class App extends React.Component {
     render() {
-        return <h1>Hello React!</h1>
+
+        return <Contador />
+
     }
 }
