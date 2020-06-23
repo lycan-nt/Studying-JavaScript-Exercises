cbb09042c9f4ca802fb5904ff0225a472dedaab6
Felipe D. Santos <lycan.felipe@gmail.com>
2020-06-22 18:35:20 -0700

    state, Props, Components

--- ReactJS/yt-react/src/App.js
+++ ReactJS/yt-react/src/App.js
@@ -2,11 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class MyButton extends Component {
+  render() {
+    return <button onClick = { () => { this.props.handleClick(this.props.label) }}> { this.props.label } </button>
+  }
+}
+
+class MyLabel extends Component {
+  render() {
+    return <p>click in the botton: {this.props.text}</p>  
+  }
+}
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labelText: "",
+    }
+  }
+
+  setLabelText = (labelText) => {
+    this.setState({ labelText });    
+  }
+
   render() {
     return (
-      <p>Hello World!</p>
+      <div className="App">
+      <MyLabel text= { this.state.labelText }/>
+      <MyButton handleClick = { this.setLabelText } label="Button 1"/>
+      <MyButton handleClick = { this.setLabelText } label="Button 2"/>
+      <MyButton handleClick = { this.setLabelText } label="Button 3"/>
+      <MyButton handleClick = { this.setLabelText } label="Button 4"/>
+
+    </div>
     )
+
    }
 }
 
