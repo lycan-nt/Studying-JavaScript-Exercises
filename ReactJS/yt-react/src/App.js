7fe10e6f272e3d931fce3f4c3f14a148d194e815
Felipe D. Santos <lycan.felipe@gmail.com>
2020-07-03 18:20:21 -0700

    Refactoring

--- ReactJS/yt-react/src/App.js
+++ ReactJS/yt-react/src/App.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
-class MyButton extends Component {
-  render() {
-    return <button onClick = { () => { this.props.handleClick(this.props.label) }}> { this.props.label } </button>
-  }
-}
+import { listRepositories } from "./api/github"
 
-class MyLabel extends Component {
-  render() {
-    return <p>click in the botton: {this.props.text}</p>  
-  }
-}
 
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      labelText: "",
-    }
-
-    this.setLabelText = this.setLabelText.bind(this);
+  this.state = {
+    data: []
+  }
+ 
   }
 
-  setLabelText(labelText) {
-    this.setState( { labelText } );    
+  componentDidMount() {
+    listRepositories()
+      .then( data => {
+        this.setState( state: { data })
+      })
   }
 
   render() {
+    const { data } = this.state
+
     return (
       <div className="App">
-      <MyLabel text= { this.state.labelText }/>
-      <MyButton handleClick = { this.setLabelText } label="Button 1"/>
-      <MyButton handleClick = { this.setLabelText } label="Button 2"/>
-      <MyButton handleClick = { this.setLabelText } label="Button 3"/>
-      <MyButton handleClick = { this.setLabelText } label="Button 4"/>
-
-    </div>
+        <ul>
+        { data.map(item => <li>{item.fu}</li> ) }
+        </ul>
+      </div>
     )
 
    }
