2578229069e13a0a1f21207c778e9b2319008776
Felipe D. Santos <lycan.felipe@gmail.com>
2020-07-08 23:26:14 -0300

    Optional Chaining

---
+++ ES2020/Optional-chaining/data.js
@@ -0,0 +1,21 @@
+export const data = {
+    user: [{
+        name: "Felipe",
+        favorite_artists: [{
+            name: "NOFX",
+            info: {
+                gender: "punk"
+            }
+            
+        },
+        {
+            name: "Death",
+            info: {
+                gender: "death metal"
+            }
+        },
+        
+        
+    ]
+    }]
+}
