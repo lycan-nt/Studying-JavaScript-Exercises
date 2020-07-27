6ba17e01194a630494a31284132602e237c90873
Felipe D. Santos <lycan.felipe@gmail.com>
2020-07-27 06:29:33 -0300

    Processando Sass com Gulp 1

---
+++ Curso-Web-Moderno/gulp/css/gulpfile.js
@@ -0,0 +1,17 @@
+const { series } = require("gulp");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglifycss = require("gulp-uglifycss");
+const contat = require("gulp-concat");
+
+function transformcaoCSS() 
+{
+    return gulp.src("src/sass/index.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(uglifycss({ "uglycomments": true }))
+        .pipe(contat("estilo.min.css"))
+        .pipe(gulp.dest("build/css"))
+}
+
+
+exports.default = series(transformcaoCSS);
