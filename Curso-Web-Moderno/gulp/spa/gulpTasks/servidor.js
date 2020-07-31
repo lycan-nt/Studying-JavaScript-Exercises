afd132e3ace2252ebbab9f80ad9957bd238d7fcf
Felipe D. Santos <lycan.felipe@gmail.com>
2020-07-31 06:29:51 -0300

    Estrutura da Build

---
+++ Curso-Web-Moderno/gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,16 @@
+const gulp = require('gulpe');
+
+function monitorarArquivos(cb)
+{
+    return cb();
+}
+
+function servidor(cb)
+{
+    return cb();
+}
+
+module.exports = {
+    monitorarArquivos,
+    servidor
+}
