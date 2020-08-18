307220748d870d3339c0463101bbb51ae778ba87
Felipe D. Santos <lycan.felipe@gmail.com>
2020-08-18 17:03:20 -0300

    Express in NodeJS

--- Digital-Innovation-one/NodeJS/script.js
+++ Digital-Innovation-one/NodeJS/script.js
@@ -1,1 +1,12 @@
-console.log('Server On!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+
+app.listen(3000, () => console.log('Server Start!'));
