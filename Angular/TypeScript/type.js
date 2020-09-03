44212b47d5bf654f21faa4d3f114be340eb78046
Felipe D. Santos <lycan.felipe@gmail.com>
2020-09-03 17:13:34 -0300

    Angular/TypeScript - Aula 3

--- Angular/TypeScript/type.js
+++ Angular/TypeScript/type.js
@@ -1,18 +1,13 @@
-var CardSuits;
-(function (CardSuits) {
-    CardSuits[CardSuits["Clubs"] = 0] = "Clubs";
-    CardSuits[CardSuits["Hearts"] = 1] = "Hearts";
-    CardSuits[CardSuits["Spades"] = 2] = "Spades";
-    CardSuits[CardSuits["Diamonds"] = 3] = "Diamonds";
-})(CardSuits || (CardSuits = {}));
-function teste() {
-    var x = 1;
-    if (x === CardSuits.Clubs) {
-        alert('True');
-    }
-    else {
-        alert('False');
-    }
-    console.log(CardSuits.Clubs);
+var obj = {
+    name: 'Felipe',
+    age: 25,
+    active: true
+};
+var obj2 = {
+    name: 'Marcella',
+    age: 24
+};
+function teste(nome, sobrenome) {
+    alert(nome + sobrenome);
 }
-teste();
+teste('Felipe', 'Santos');
--- Angular/TypeScript/type.ts
+++ Angular/TypeScript/type.ts
@@ -1,15 +1,16 @@
-enum CardSuits {Clubs, Hearts, Spades, Diamonds}
-
-function teste() {
-    let x = 1;
-    if (x === CardSuits.Clubs)
-    {
-        alert('True');
-    }
-    else{
-        alert('False')
-    }
-    console.log(CardSuits.Clubs);
+let obj: {name: string, age: number, active?: boolean } = {
+    name: 'Felipe',
+    age: 25,
+    active: true
 }
 
-teste();
+let obj2: {[key: string] : any} = {
+    name: 'Marcella',
+    age: 24
+}
+
+function teste(nome: string , sobrenome?: string) {
+    alert(nome + sobrenome);
+}
+
+teste('Felipe', 'Santos');
