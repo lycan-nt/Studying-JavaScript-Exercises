271c5cc55300e91237954ae588b136235d956857
lycan-nt <lycan.felipe@gmail.com>
2021-03-18 22:04:14 -0300

    Pipes

--- Angular/Angular-University/1-components/src/app/app.component.ts
+++ Angular/Angular-University/1-components/src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  startDate = new Date(2000, 0, 1);
+
+  price = 9.99;
+
   onCourseSelected(course: Course) {
     console.log("Card Clicked burbuled...", course);
   }
