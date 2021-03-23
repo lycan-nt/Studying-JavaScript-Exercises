948134cf8920e044b9833e7186368677b8e23b58
lycan-nt <lycan.felipe@gmail.com>
2021-03-22 21:00:55 -0300

    Add Server-3

---
+++ Angular/Angular-University/0-angular-course/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  data = {
+    title: 'Angular Core Deep Dive'
+  }
+
+  onLogoClicked()
+  {
+    alert("Clicked");
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle; 
+  }
+}
