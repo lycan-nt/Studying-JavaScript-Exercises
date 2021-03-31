bdf3055a213c01213f1692e4f6afbf38de5d3bf3
lycan-nt <lycan.felipe@gmail.com>
2021-03-30 21:56:49 -0300

    Observable

--- Angular/Angular-RXJS/angular-observable-rxjs/src/app/app.component.ts
+++ Angular/Angular-RXJS/angular-observable-rxjs/src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { timer, of, Observable } from 'rxjs';
+
+// const source = timer(1000, 2000);
+// const subscribe = source.subscribe(val => console.log(val));
+
+// setTimeout(() => { subscribe.unsubscribe(); }, 10000);
+
+const myObservable = of('Felipe', 'Marcella', 'Bryan');
+
+const myObserver = {
+  next: (x: string) => console.log('Observer go to a next value: ' + x),
+  error: (err: string) => console.error('Observer go to a error: ' + err),
+  complete: () => console.log('Observer go to a complete notification'),
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor()
+  {
+    myObservable.subscribe(myObserver);
+  }
   title = 'angular-observable-rxjs';
 }
